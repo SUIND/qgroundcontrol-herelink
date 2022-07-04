55517f744e2ce04d12883629235bc687b7fc10a8
Michael Oborne <mich146@hotmail.com>
2022-07-04 16:44:12 +1000

    offset-to-zero and streamable

--- src/VideoStreaming/VideoReceiver.cc
+++ src/VideoStreaming/VideoReceiver.cc
@@ -751,6 +751,8 @@ VideoReceiver::startRecording(const QString &videoFile)
     }
     emit videoFileChanged();
 
+    g_object_set(static_cast<gpointer>(_sink->mux), "offset-to-zero", true, nullptr);
+    g_object_set(static_cast<gpointer>(_sink->mux), "streamable", true, nullptr);
     g_object_set(static_cast<gpointer>(_sink->filesink), "location", qPrintable(_videoFile), nullptr);
     qCDebug(VideoReceiverLog) << "New video file:" << _videoFile;
 
